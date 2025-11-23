f3bd36708696a74e8b901e30f25e6b59f7e371ae
Bhupesh Nemade <bhupesh.nemade25@gmail.com>
2025-11-23 13:16:31 +0530

    added node ci

---
+++ test/sum.test.js
@@ -0,0 +1,8 @@
+const sum = require('../index');
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+test('add negative numbers', () => {
+  expect(sum(-2, -3)).toBe(-5);
+})
